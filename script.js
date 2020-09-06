ce99be35f2613589d3fa376ffb8eb88eda1e95cd
paalso <40a.pvl@gmail.com>
2020-09-06 11:19:30 +0300

    Ver 1.0. In general, all that was required done

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 const DEFAULT_PYRAMID_HEIGHT = 8;
 const DEFAULT_PYRAMID_SYMBOL = "#";
 
-const generatePyramidLine = (row, height, symblol = "#") => {
+const generatePyramidLine = (row, height, symblol=DEFAULT_PYRAMID_SYMBOL) => {
     const paraElement = document.createElement("p");
     paraElement.appendChild(
-        document.createTextNode(" ".repeat(height - row) + symblol.repeat(row + 1)));
+        document.createTextNode("\xa0".repeat(height - row) + symblol.repeat(row + 1)));
     return paraElement;
 }
 
 const printPyramid = (element, height, symbol) => {
     element.innerHTML = "";
+    const pyramidContainerElement = document.createElement("div");
+    pyramidContainerElement.setAttribute("class", "pyramid-container");
     for (let i = 1; i <= height; ++i)
-        element.appendChild(generatePyramidLine(i, height, symbol));
+        pyramidContainerElement.appendChild(generatePyramidLine(i, height, symbol));
+    element.appendChild(pyramidContainerElement);
 }
 
 const heightElement = document.getElementById("pyramyd-height");
 const heightValueElement = document.getElementById("height-value");
 const symbolElement = document.getElementById("pyramid-symbol");
-const pyramidContainerElement = document.querySelector(".pyramid-container");
+const pyramidAreaElement = document.querySelector(".pyramid-area");
 
 heightElement.setAttribute("value", DEFAULT_PYRAMID_HEIGHT);
 heightValueElement.textContent = DEFAULT_PYRAMID_HEIGHT;
 
 heightElement.addEventListener(
-    "change", e => {
+    "input", e => {
         heightValueElement.textContent = heightElement.value;
-        printPyramid(pyramidContainerElement, heightElement.value, symbolElement.value);
+        printPyramid(pyramidAreaElement, heightElement.value, symbolElement.value);
     }
 );
 
 symbolElement.addEventListener(
     "change", e =>
-        printPyramid(pyramidContainerElement, heightElement.value, symbolElement.value)
+        printPyramid(pyramidAreaElement, heightElement.value, symbolElement.value)
 );
 
-printPyramid(pyramidContainerElement, DEFAULT_PYRAMID_HEIGHT, DEFAULT_PYRAMID_SYMBOL);
+printPyramid(pyramidAreaElement, DEFAULT_PYRAMID_HEIGHT, DEFAULT_PYRAMID_SYMBOL);
